a9fe25d009f929af2e5f22376d5a8e4f10538f05
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-15 10:56:57 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+	def main():
+    pass
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
+if __name__ == '__main__':
+    main()
---
+++ hello.py
@@ -0,0 +1,5 @@
+def main():
+    for i in range(5):
+        print("Hello")
+if __name__ == '__main__':
+    main()
