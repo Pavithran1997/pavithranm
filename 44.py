1c5adb79b600cb16515e0f56dcefd1477e7b7f27
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-15 11:24:51 +0530

    Add files via upload

---
+++ 44.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      Guest
+#
+# Created:     14/02/2018
+# Copyright:   (c) Guest 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    pass
+    y = input("Enter a character: ")
+
+    if y in ('a', 'e', 'i', 'o', 'u'):
+        print(y, "is a Vowel")
+    else:
+        print(y,"is a constant")
+
+if __name__ == '__main__':
+    main()
