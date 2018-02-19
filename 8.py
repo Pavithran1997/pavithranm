86deb6fa88c87b2e9363a591f911fc81ac69dae8
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-19 17:36:28 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,9 @@
+def main():
+     n=int(input("Enter a number: "))
+     sum1 = 0
+     while(n > 0):
+          sum1=sum1+n
+          n=n-1
+     print("The sum of first n natural numbers is",sum1)
+if __name__ == '__main__':
+    main()
