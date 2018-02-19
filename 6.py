4ed93803471f64f9dbdaef9873f2911f0190683f
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-19 17:30:07 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,15 @@
+  def main():
+    pass
+    year = int(input("Please Enter the Year Number you wish: "))
+
+    if (year%400 == 0):
+             print("%d is a Leap Year" %year)
+    elif (year%100 == 0):
+            print("%d is Not the Leap Year" %year)
+    elif (year%4 == 0):
+            print("%d is a Leap Year" %year)
+    else:
+              print("%d is Not the Leap Year" %year)
+
+if __name__ == '__main__':
+    main()
