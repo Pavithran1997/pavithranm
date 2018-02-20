c5bd28ec2e519efad2c51403f50b8b18fdbcef51
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-20 17:06:30 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -6,8 +6,8 @@ for i in range(2,a//2+1):
     if(a%i==0):
         k=k+1
 if(k<=0):
-    print("It's a prime")
+    print("Yes")
 else:
-    print("It is not a prime")
+    print("No")
 if __name__ == '__main__':
     main()
