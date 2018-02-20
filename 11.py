0fd37eeba97d74cdefe7f4d31c0faae5290277b9
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-20 17:48:06 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,9 +1,9 @@
 def main():
-    pass
+     pass
 def power(base,exp):
-    if(exp==1):
+     if(exp==1):
         return(base)
-    if(exp!=1):
+     if(exp!=1):
         return(base*power(base,exp-1))
 base=int(input("Enter base: "))
 exp=int(input("Enter exponential value: "))
