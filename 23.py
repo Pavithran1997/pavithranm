60210e6bc9f59e1c1948ee891fc390ec90cb8948
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-22 17:45:19 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,11 @@
+def main():
+    pass
+lee=[]
+n=int(input("enter the element in array\n"))
+for i in range(0,n):
+	b=int(input())
+	lee.append(b)
+lee.sort()
+print(lee[0])
+if __name__ == '__main__':
+    main()
