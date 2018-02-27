7feb86666476512d6398f6cc3885bb9ae7c21615
Pavithran1997 <36472366+Pavithran1997@users.noreply.github.com>
2018-02-27 17:31:57 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,11 @@
+def main():
+    pass
+n=input("enter n value\n")
+sdd=[]
+for i in range(0,n):
+	b=int(input())
+	sdd.append(b)
+for i in range(0,n):
+	print(i,sdd[i])
+if __name__ == '__main__':
+    main()
